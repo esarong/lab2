7e1a4e8e5e5264c182d21452f4ffea089470f33e
esarong <esarong@student.apc.edu.ph>
2023-01-15 15:25:19 +0800

    week 6 commit 11pm

--- week5/myscript.js
+++ week5/myscript.js
@@ -1,7 +1,9 @@
+alert("Do not expect anything on this page");
+
+
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
 
-  alert("Do not expect anything on this page");
-
+  
 
